1871d6cdad7cf4e390a851a2b1c944590cd059d4
Pawan Kumar <cnct.pawan@gmail.com>
2025-01-23 01:00:13 +0530

    added models

---
+++ backend/models/product.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+});
